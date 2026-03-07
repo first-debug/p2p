efdc18b0ff0e85c90cce6b5aa23e9a33759202b2
vladi <vladislavvasilev53@gmail.com>
2026-03-08 01:33:46 +0300

    feat(peers): add commands for show peer list and connect to them
    Available commands:
    - `list peers`: return list of available peers.
    - `connect <peer-id>`: expose new connection to peer.
    
    IMPORTANT: logic for sendinging messages from `scanner` to cahnnel
    doesn't implemented yet. Call `connect <peer-ip>` will create a new
    session and return to read default commands.

--- internal/manager/cli/manager.go
+++ internal/manager/cli/manager.go
@@ -3,14 +3,17 @@ package cli
 import (
 	"bufio"
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	client "github.com/first-debug/p2p/internal/client"
 	"github.com/first-debug/p2p/internal/domain"
 	"github.com/first-debug/p2p/internal/manager"
 	peerstorage "github.com/first-debug/p2p/internal/storage/peer"
 	sessionstorage "github.com/first-debug/p2p/internal/storage/session"
+	"github.com/google/uuid"
 )
 
 var logger log.Logger = *log.New(os.Stderr, "[CliManager] ", log.LstdFlags)
@@ -43,6 +46,49 @@ func (m *CliManager) Run() error {
 			return nil
 		default:
 			if scanner.Scan() {
+				input := scanner.Text()
+				if input == "list peers" {
+					peers, err := m.pStorage.GetAll()
+					if err != nil {
+						fmt.Fprintln(writer, err.Error())
+					} else {
+						fmt.Fprint(writer, "[")
+						for _, v := range peers {
+							fmt.Fprintln(writer)
+							fmt.Fprintln(writer, "\t", v.ID)
+						}
+						fmt.Fprintln(writer, "]")
+					}
+				} else if strings.Contains(input, "connect ") {
+					strs := strings.Split(input, " ")
+					if len(strs) != 2 {
+						fmt.Fprintln(writer, "too many arguments for `connect` command")
+						continue
+					}
+					id, err := uuid.Parse(strs[1])
+					if err != nil {
+						fmt.Fprintln(writer, err.Error())
+						continue
+					}
+					peer, err := m.pStorage.GetByID(id)
+					if err != nil {
+						fmt.Fprintln(writer, err.Error())
+						continue
+					}
+					sess, err := m.client.Connect(context.Background(), &peer)
+					if err != nil {
+						fmt.Fprintln(writer, err.Error())
+						continue
+					}
+					ch, err := sess.GetWriteChannel(m.ctx)
+					if err != nil {
+						fmt.Fprintln(writer, err.Error())
+						continue
+					}
+					// TODO: add logic for sending messages from the `scanner` to the channel
+				} else {
+					fmt.Fprintln(writer, "not supported command")
+				}
 			}
 		}
 	}
